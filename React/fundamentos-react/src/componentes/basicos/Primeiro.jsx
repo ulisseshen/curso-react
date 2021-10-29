03912fb15e4edbc24db053a4b59db97e39ac8ea5
Ulisses Hen <ulisseshen@gmail.com>
2021-10-29 13:35:33 -0300

    alterado diretorio do curso

--- React/fundamentos-react/src/componentes/basicos/Primeiro.jsx
+++
@@ -1,9 +0,0 @@
-export default function Primeiro() {
-    const mensagem = 'Seja bem vindo(a)!'
-    return (
-        <div>
-            <h2>Primeiro componente</h2>
-            <p> { mensagem } </p>
-        </div>
-    )
-}
